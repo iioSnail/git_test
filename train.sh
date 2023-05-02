7919f0bffc9ebfc1361b239d7fd87a3e8a968ca2
iiosnail <iiosnail@163.com>
2023-05-02 12:41:09 +0800

    commit

---
+++ train.sh
@@ -0,0 +1,7 @@
+python c_train.py --datas wang271k,sighan15train,sighan14train,sighan13train,sighan15train,sighan14train,sighan13train \
+                  --batch-size 32 \
+                  --valid-ratio 0.05 \
+                  --resume \
+                  --epochs 20 \
+                  --model MultiModalMyModel \
+                  --ckpt-dir ../drive/MyDrive/Lightning/MyModel/
